7668cb6b20f2cd361c239c5fb98a8a364a25d0fa
Brionesgmm <99777937+Brionesgmm@users.noreply.github.com>
2023-05-23 22:30:56 -0600

    chat socketio now has a database attached

--- socket-react-app/index.js
+++ socket-react-app/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const socketio = require("socket.io");
+const mongoose = require("mongoose");
+const Message = require("./models/Message");
 
 const app = express();
 const server = http.createServer(app);
@@ -11,13 +13,42 @@ const io = socketio(server, {
   },
 });
 
+mongoose
+  .connect(
+    "mongodb+srv://jbrionesgmm:Brucelee7!S@testchat.c4ssd1f.mongodb.net/",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log("MongoDB connection error: ", err));
+
 app.use(cors());
 
 io.on("connection", (socket) => {
   console.log("New client connected");
 
-  socket.on("message", (message) => {
-    io.emit("message", message);
+  Message.find()
+    .sort("-timestamp")
+    .limit(50)
+    .then((messages) => {
+      console.log("Fetched messages: ", messages);
+      socket.emit("message history", messages);
+    })
+    .catch((err) => console.error(err));
+
+  socket.on("message", (content) => {
+    console.log("Received message content: ", content);
+    const message = new Message({ content });
+
+    message
+      .save()
+      .then((savedMessage) => {
+        console.log("Saved message: ", savedMessage);
+        io.emit("message", savedMessage);
+      })
+      .catch((err) => console.error(err));
   });
 
   socket.on("disconnect", () => {
@@ -25,6 +56,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(process.env.PORT || 5000, () =>
+  console.log(`Server has started on port ${process.env.PORT || 5000}`)
+);
